c1dbad77f3ca6d8fab83a6dda716846d6afe2ae3
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-05-05 08:21:00 -0300

    create erc20Token entity when it doesn't exist

--- src/aludelv1.5.ts
+++ src/aludelv1.5.ts
@@ -1,16 +1,53 @@
-import { BigInt, log, store, Value } from "@graphprotocol/graph-ts";
+import { Address, BigInt, log, store, Value } from "@graphprotocol/graph-ts";
 
 import {
   RewardClaimed
 } from "../generated/templates/AludelV15Template/AludelV15";
 import { ERC20 } from "../generated/CrucibleFactory/ERC20";
 
-import { CrucibleEntity, Leaderboard, Reward } from "../generated/schema";
+import { CrucibleEntity, ERC20Token, Leaderboard, Reward } from "../generated/schema";
 import { getCrucibleId, getRewardId } from "./utils";
+
+export function createERC20Token(address: Address): ERC20Token {
+  let tokenContract = ERC20.bind(address)
+  let token = new ERC20Token(address.toHexString())
+
+  token.address = address
+  let decimals = tokenContract.try_decimals()
+  if (decimals.reverted) {
+    log.error("createERC20Token: failed get token decimals: {}", [address.toHexString()]);
+    // reviewme: this behavior may not be ideal.
+    token.decimals = BigInt.fromI32(0)
+  } else {
+    token.decimals = BigInt.fromI32(decimals.value)
+  }
+
+  let name = tokenContract.try_name()
+  if (name.reverted) {
+    log.error("createERC20Token: failed get token name: {}", [address.toHexString()]);
+    // reviewme: this behavior may not be ideal.
+    token.name = ''
+  } else {
+    token.name = name.value
+  }
+
+  let symbol = tokenContract.try_symbol()
+  if (symbol.reverted) {
+    log.error("createERC20Token: failed get token symbol: {}", [address.toHexString()]);
+    // reviewme: this behavior may not be ideal.
+    token.symbol = ''
+  } else {
+    token.symbol = symbol.value
+  }
+
+  token.save()
+
+  return token
+}
 
 export function handleRewardClaimed(event: RewardClaimed): void {
   let aludel = event.address
-  let token = event.params.token
+  let tokenAddress = event.params.token
   let amount = event.params.amount
   let crucibleAddress = event.params.vault
 
@@ -25,26 +62,22 @@ export function handleRewardClaimed(event: RewardClaimed): void {
   }
   // log.warning("rewardClaimed: crucible {} loaded", [crucibleId])
 
-  let rewardId = getRewardId(crucibleAddress, aludel, token)
+  let erc20Token = ERC20Token.load(tokenAddress.toHexString())
+  if (erc20Token == null) {
+    erc20Token = createERC20Token(tokenAddress)
+  }
+
+  let rewardId = getRewardId(crucibleAddress, aludel, tokenAddress)
   let reward = Reward.load(rewardId)
   if (reward == null) {
     reward = new Reward(rewardId)
-    reward.token = token
     reward.amount = amount
+    reward.token = tokenAddress.toString()
     reward.aludel = aludel
     reward.crucible = crucible.id
     // log.warning('newReward: {}', [rewardId])
     crucible.rewardsLength = crucible.rewardsLength.plus(BigInt.fromI32(1))
     
-    let tokenContract = ERC20.bind(token)
-    let decimals = tokenContract.try_decimals()
-    if (decimals.reverted) {
-      log.error("rewardClaimed: failed get token decimals: {}", [token.toHexString()]);
-      // reviewme: this behavior may not be ideal.
-      reward.tokenDecimals = BigInt.fromI32(18)
-    } else {
-      reward.tokenDecimals = BigInt.fromI32(decimals.value)
-    }
   } else {
     reward.amount = reward.amount.plus(amount)
     // log.warning('updateReward: {}', [rewardId])
