694ee20724e44d2c208a138baac3a803d9da0cd6
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-05-06 17:55:49 -0300

    add RewardClaimed legacy handler

--- src/aludelv1.5.ts
+++ src/aludelv1.5.ts
@@ -1,61 +1,91 @@
-import { Address, BigInt, log, store, Value } from "@graphprotocol/graph-ts";
+import {
+  Address,
+  BigInt,
+  ethereum,
+  log,
+  store,
+  Value,
+} from "@graphprotocol/graph-ts";
+
+import { RewardClaimed } from "../generated/templates/AludelV15Template/AludelV15";
 
 import {
-  RewardClaimed
-} from "../generated/templates/AludelV15Template/AludelV15";
-
-import { CrucibleEntity, ERC20Token, Leaderboard, Reward } from "../generated/schema";
+  CrucibleEntity,
+  ERC20Token,
+  Leaderboard,
+  Reward,
+} from "../generated/schema";
 import { getCrucibleId, getRewardId, getTokenId } from "./utils";
 import { createERC20Token } from "./erc20Token";
 
-export function handleRewardClaimed(event: RewardClaimed): void {
-  let aludel = event.address
-  let tokenAddress = event.params.token
-  let amount = event.params.amount
-  let crucibleAddress = event.params.vault
-
-  // log.warning("prereward: {} {} {} {}", [crucibleAddress.toHex(), aludel.toHex(), token.toHex(), amount.toString()])
-
-  let crucibleId = getCrucibleId(crucibleAddress)
+export function _handleRewardClaimed(
+  event: ethereum.Event,
+  aludel: Address,
+  tokenAddress: Address,
+  amount: BigInt,
+  crucibleAddress: Address
+): void {
+  let crucibleId = getCrucibleId(crucibleAddress);
   // load crucible
   let crucible = CrucibleEntity.load(crucibleId);
   if (crucible == null) {
     log.error("rewardClaimed: crucible {} not found", [crucibleId]);
-    return
+    return;
   }
   // log.warning("rewardClaimed: crucible {} loaded", [crucibleId])
 
-  let tokenId = getTokenId(tokenAddress)
-  let erc20Token = ERC20Token.load(tokenId)
+  let tokenId = getTokenId(tokenAddress);
+  let erc20Token = ERC20Token.load(tokenId);
   if (erc20Token == null) {
-    erc20Token = createERC20Token(tokenAddress)
+    erc20Token = createERC20Token(tokenAddress);
   }
 
-  let rewardId = getRewardId(crucibleAddress, aludel, tokenAddress)
-  let reward = Reward.load(rewardId)
+  let rewardId = getRewardId(crucibleAddress, aludel, tokenAddress);
+  let reward = Reward.load(rewardId);
   if (reward == null) {
-    reward = new Reward(rewardId)
-    reward.amount = amount
-    reward.token = tokenId
-    reward.aludel = aludel
-    reward.crucible = crucible.id
+    reward = new Reward(rewardId);
+    reward.amount = amount;
+    reward.token = tokenId;
+    reward.aludel = aludel;
+    reward.crucible = crucible.id;
     // log.warning('newReward: {}', [rewardId])
-    crucible.rewardsLength = crucible.rewardsLength.plus(BigInt.fromI32(1))
-    
+    crucible.rewardsLength = crucible.rewardsLength.plus(BigInt.fromI32(1));
   } else {
-    reward.amount = reward.amount.plus(amount)
+    reward.amount = reward.amount.plus(amount);
     // log.warning('updateReward: {}', [rewardId])
   }
-  reward.lastUpdate = event.block.timestamp
-  reward.save()
+  reward.lastUpdate = event.block.timestamp;
+  reward.save();
 
-  crucible.save()
+  crucible.save();
 
-  let leaderboard = Leaderboard.load(crucibleId)
+  let leaderboard = Leaderboard.load(crucibleId);
   if (leaderboard == null) {
     log.error("rewardClaimed: failed to load leaderboard {}", [crucibleId]);
-    return
+    return;
   }
-  leaderboard.points = leaderboard.points.plus(amount)
-  leaderboard.save()
+  leaderboard.points = leaderboard.points.plus(amount);
+  leaderboard.save();
+}
+
+export function handleRewardClaimed(event: RewardClaimed): void {
+  let aludel = event.address;
+  let tokenAddress = event.params.token;
+  let amount = event.params.amount;
+  let crucibleAddress = event.params.vault;
+
+  // log.warning("prereward: {} {} {} {}", [crucibleAddress.toHex(), aludel.toHex(), token.toHex(), amount.toString()])
+
+  _handleRewardClaimed(event, aludel, tokenAddress, amount, crucibleAddress);
+}
+
+export function handleRewardClaimedLegacy(event: RewardClaimed): void {
+  let aludel = event.address;
+  let tokenAddress = event.params.token;
+  let amount = event.params.amount;
+  let crucibleAddress = event.params.vault;
+
+  // log.warning("prereward: {} {} {} {}", [crucibleAddress.toHex(), aludel.toHex(), token.toHex(), amount.toString()])
+
+  _handleRewardClaimed(event, aludel, tokenAddress, amount, crucibleAddress);
 }
