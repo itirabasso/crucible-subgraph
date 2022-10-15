88710ccbb187f6d5643623f4242151174514d445
Ignacio Mariano Tirabasso <ignacio.tirabasso@gmail.com>
2022-10-15 04:29:46 -0300

    Update AludelFactory ABI (#5)
    * Deploy into test-2 on every push to main (#5)
    
    * Update node.js.yml
    
    * update goerli config
    
    * update AludelFactory abi
    
    * update to latest factory address
    
    * update mappings
    
    * remove deprecated testnets and  add goerli
    
    * bump graph-cli and graph-ts
    
    * update yarn.lock
    
    * remove InstanceRegistry events, add ProgramAdded event handler
    
    * update abis & config
    
    * add ProgramDelisted event in AludelFactory ABI
    
    * add ProgramDelisted handler
    
    * add ProgramDelisted to subgraph yaml
    
    * downgrade graph-ts version
    
    * Fix: we use the real disabled value of the template
    
    Co-authored-by: paco <plaintextpaco@gmx.com>
    Co-authored-by: plaintextpaco <111788132+plaintextpaco@users.noreply.github.com>

--- src/aludelFactory.ts
+++ src/aludelFactory.ts
@@ -1,18 +1,19 @@
 import { dataSource, log, store } from "@graphprotocol/graph-ts";
-import { AludelFactory, InstanceAdded, InstanceRemoved, TemplateAdded, TemplateUpdated } from "../generated/AludelFactory/AludelFactory";
+import { AludelFactory, ProgramAdded, ProgramDelisted, TemplateAdded, TemplateUpdated } from "../generated/AludelFactory/AludelFactory";
 import { RewardProgram, Template } from "../generated/schema";
 import { AludelV15Template } from "../generated/templates";
 import { AludelV15 } from "../generated/templates/AludelV15Template/AludelV15";
 import { getAludelId, getIdFromAddress } from "./utils";
 
-export function handleInstanceAdded(event: InstanceAdded): void {
-  let aludelAddress = event.params.instance
+export function handleProgramAdded(event: ProgramAdded): void {
+  let aludelAddress = event.params.program
   AludelV15Template.create(aludelAddress)
+
   let aludelId = getAludelId(aludelAddress)
 
 
   let factory = AludelFactory.bind(dataSource.address())
-  let data = factory.try_getProgram(aludelAddress)
+  let data = factory.try_programs(aludelAddress)
   if (data.reverted) {
     log.error("handleInstanceAdded: failed get program data: {}", [aludelAddress.toHexString()]);
     return;
@@ -35,10 +36,12 @@ export function handleInstanceAdded(event: InstanceAdded): void {
   rewardProgram.save()
  }
 
- export function handleInstanceRemoved(event: InstanceRemoved): void {
-  let aludelAddress = event.params.instance
-  AludelV15Template.create(aludelAddress)  
+// we need a remove instance / delist aludel event emitted
+ export function handleProgramDelisted(event: ProgramDelisted): void {
+  let aludelAddress = event.params.program
+  // AludelV15Template.create(aludelAddress)
   let aludelId = getAludelId(aludelAddress)
+  log.warning("delisting program {}", [aludelAddress.toString(), aludelId.toString()])
   store.remove('RewardProgram', aludelId)
  }
 
@@ -52,19 +55,21 @@ export function handleTemplateAdded(event: TemplateAdded): void {
   let data = factory.try_getTemplate(event.params.template)
   if (data.reverted) {
     log.error("handleInstanceAdded: failed get template data", []);
+    return;
   }
 
-  template.disabled = false
+  template.disabled = data.value.disabled
   template.name = data.value.name
   template.save()
 }
-export function handleTemplateUpdated(event: TemplateUpdated): void {
+export function handleTemplateChanged(event: TemplateUpdated): void {
   let id = getIdFromAddress(event.params.template);
   let template = Template.load(id)
   if (template === null) {
     log.error('handleTemplateUpdate: cannot load template with id', [id])
     return;
   }
+
   template.disabled = event.params.disabled
   template.save()
 }
